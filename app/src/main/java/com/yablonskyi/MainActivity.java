7aabede3ff8ff967d2e31fba5e87c2e99785c602
Marvin99 <maksym.yablonskyi@gmail.com>
2018-09-06 10:58:45 +0300

    Lab 1-2

---
+++ app/src/main/java/com/yablonskyi/MainActivity.java
@@ -0,0 +1,35 @@
+package com.yablonskyi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonHi, buttonClear;
+    EditText inputName;
+    TextView myName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello World");
+        inputName = findViewById(R.id.inputName);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonHi = findViewById(R.id.buttonHi);
+        myName = findViewById(R.id.myname);
+    }
+
+    public void clearMessage(View view) {
+        inputName.getText().clear();
+    }
+
+    public void showMessage(View view) {
+        myName.setText("Hello" + " " + inputName.getText().toString());
+        inputName.getText().clear();
+    }
+}
